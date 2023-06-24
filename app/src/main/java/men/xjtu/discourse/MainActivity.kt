f8b4cf8a44d18f64c38e49ed10964aff66300836
xjtu-men <136139347+xjtu-men@users.noreply.github.com>
2023-06-24 15:34:10 +0800

    resolve (*.)xjtu.men using secure DNS (DoH)

--- app/src/main/java/men/xjtu/discourse/MainActivity.kt
+++ app/src/main/java/men/xjtu/discourse/MainActivity.kt
@@ -1,13 +1,15 @@
 package men.xjtu.discourse
 
+import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.KeyEvent
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat.startActivity
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,11 +20,12 @@ class MainActivity : AppCompatActivity() {
 
         val myWebView: WebView = findViewById(R.id.webview)
         myWebView.settings.javaScriptEnabled = true
-        myWebView.webViewClient = WebViewClient()
+        myWebView.webViewClient = MyWebViewClient(this)
 
         myWebView.loadUrl("https://xjtu.men")
 
     }
+
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
         val myWebView: WebView = findViewById(R.id.webview)
         // Check if the key event was the Back button and if there's history
@@ -38,6 +41,42 @@ class MainActivity : AppCompatActivity() {
         // system behavior (probably exit the activity)
         return super.onKeyDown(keyCode, event)
     }
+}
+
+fun IsXjtuMenService(string: String): Boolean {
+    return try {
+        Uri.parse(string).host.toString().endsWith("xjtu.men")
+    } catch (e: Exception) {
+        e.printStackTrace();
+        false
+    }
+}
+
+class MyWebViewClient(private val mainActivity: MainActivity) : WebViewClient() {
+    override fun shouldInterceptRequest(
+        view: WebView?,
+        request: WebResourceRequest?
+    ): WebResourceResponse? {
+        return try {
+            when {
+                // intercept XJTU.MEN traffic to use DNS over HTTPS
+                IsXjtuMenService(request?.url.toString()) -> WebViewNetworkHandler(request)
+                else -> null
+            }
+        } catch (e: Exception) {
+            e.printStackTrace();
+            null
+        }
+    }
+
+    override fun shouldOverrideUrlLoading(webView: WebView?, url: String): Boolean {
+        if (IsXjtuMenService(url)) return false
+        // open non XJTU.MEN links in system browser
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        mainActivity.startActivity(intent)
+        return true
+    }
 
 }
+
 
---
+++ app/src/main/java/men/xjtu/discourse/WebViewNetworkHandler.kt
@@ -0,0 +1,69 @@
+package men.xjtu.discourse
+
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
+import okhttp3.Cache
+import okhttp3.Dns
+import okhttp3.Headers
+import okhttp3.Headers.Companion.toHeaders
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.dnsoverhttps.DnsOverHttps
+import java.io.File
+import java.net.InetAddress
+import java.net.UnknownHostException
+import java.nio.charset.Charset
+
+// From
+// https://stackoverflow.com/questions/68449855/how-do-i-override-the-dns-resolution-of-an-android-webview
+// https://stackoverflow.com/questions/65420610/how-to-implement-a-custom-dns-for-okhttp3
+object WebViewNetworkHandler {
+    private var client: OkHttpClient? = null
+
+    operator fun invoke(webResourceRequest: WebResourceRequest?): WebResourceResponse? {
+        val url: String = webResourceRequest?.url.toString()
+        val headers: Headers? = webResourceRequest?.requestHeaders?.toHeaders()
+
+        val newRequest = headers?.let {
+            Request.Builder()
+                .url(url)
+                .headers(it)
+                .build()
+        }
+
+        val response = newRequest?.let { getClient().newCall(newRequest).execute() }
+
+        return response?.let { it ->
+            WebResourceResponse(
+                it.body.contentType()?.let { "${it.type}/${it.subtype}" },
+                it.body.contentType()?.charset(Charset.defaultCharset())?.name(),
+                it.code,
+                "OK",
+                it.headers.toMap(),
+                it.body.byteStream()
+            )
+        }
+    }
+
+    private fun getClient() = client ?: makeClient().also { client = it }
+
+    private fun makeClient(): OkHttpClient {
+
+        val appCache = Cache(File("cacheDir", "okhttpcache"), 10 * 1024 * 1024)
+
+        val bootstrapClient = OkHttpClient.Builder()
+            .cache(appCache)
+            .build()
+
+
+        val dns = DnsOverHttps.Builder().client(bootstrapClient)
+            .url("https://1.1.1.1/dns-query".toHttpUrl())
+            .build()
+
+        return bootstrapClient.newBuilder()
+            .dns(dns)
+            .build()
+    }
+
+}
