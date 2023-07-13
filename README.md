a487ddbae2f68e3a59ad109b7197622e42586c6a
xjtu-men <136139347+xjtu-men@users.noreply.github.com>
2023-07-13 16:13:52 +0800

    adaptive notification bar color; change logo

--- README.md
+++ README.md
@@ -1,19 +1,49 @@
-## Naïve Android App 简易的Android客户端
+# Android Client for Discourse 
 
-用WebView套了层壳，并且使用内嵌的安全DNS解析器防校园网的**DNS污染**导致的网站打不开，
-不过直连的速度并没有变化，要快仍然需要额外的工具。
+You can use this Android client to access [XJTU.MEN](https://xjtu.men), a Discourse powered forum-like communication platform for [XJTU](https://en.wikipedia.org/wiki/Xi%27an_Jiaotong_University). 
+
+Or you can use it as a template by simply replacing `xjtu.men` with the URL of any website you like (also remember to change the launcher icon).
+
+## Features
+* (optional) support embedded secure DNS (DNS over HTTPS) using okhttp
+* support specifying which domains / URLs to open in system's default browser
+* support uploading (no storage permission required)
+[ ] support (partial / full-site) cache 
+[ ] automatically / periodically fetch new posts
+[ ] automatically connect to the optimal(closest) CDN edge server
+
+## Why not use off-the-shelf browser like Chrome / Firefox
+Considering Google Play is inaccessible and even not pre-installed  in **some** regions of the world,  Chrome and Firefox are not readily available and hard to obtain, many people are accustomed to open a service by a single click on an icon in their launcher. 
+
+For users in other regions, as Discourse supports the [Progressive Web Application](https://meta.discourse.org/t/progressive-web-application-pwa/79217) feature, you can add a launcher icon by simply clicking 'Install APP' in Chrome or in [Safari](https://meta.discourse.org/t/discourse-now-works-as-a-pwa-in-ios/146346).
+
+## Contribution and feedback
+New contributions are very welcomed. :hugs:
+
+If you have any problems, feel free to file an issue or discuss in the related posts on [XJTU.MEN](https://xjtu.men/t/topic/1440).
+
+# Discourse Android客户端
+使用Kotlin开发，用WebView套了层壳，并且使用内嵌的安全DNS解析器解决校园网**DNS污染**导致网站打不开的问题。
+
+使用客户端和直接使用浏览器相比，直连的速度并没有变化，要快仍然需要额外的工具。
+
+经过测试，Chrome会默认使用QUIC连接，相比于HTTP/2 & TLS1.3的Firefox感觉上要快一点。
 
 另请参阅：[“交大門”网站的相关主题](https://xjtu.men/t/topic/1440)
 
 ## 功能更新
 * 对于所有非xjtu.men的链接，点击后自动用**系统的默认浏览器**打开
 * ~~为了解决校园网的DNS污染，把xjtu.men到CloudFlare Edge的IP解析进行**硬编码**~~
-* 摒弃IP的硬编码，内嵌基于[okhttp](https://square.github.io/okhttp/)的DNS over HTTPS (DoH)，[1.1.1.1](https://1.1.1.1/)。
-[ ] 自动选择最快的DoH服务器，参考[DNSCrypt列表](https://github.com/DNSCrypt/dnscrypt-resolvers/)
+* 内嵌基于okhttp的DNS over HTTPS (DoH)，[1.1.1.1](https://1.1.1.1/)。
+  [ ] 自动选择最快的DoH服务器，参考[DNSCrypt列表](https://github.com/DNSCrypt/dnscrypt-resolvers/)
+  [ ] 自动选择最快的CloudFlare Edge服务器，加速网站访问
+* 由于网络是中国互联网用户的一个很大的瓶颈，将增加以下功能，优化用户体验：
+  [ ] 使用缓存，避免每次打开网页过慢。（考虑到目前内容较少，也许可以采用**全站**本地存储？）
+  [ ] 自动获取新内容，无需用户人工刷新
+* [ ] 支持上传（也许无需打开访问/storage/emulated/0/的权限，调用接口即可）
 
 ## 欢迎反馈和贡献
-由于鄙人Android开发还是3年之前学的，学艺不精，
-如果不好用，欢迎在Github提Issue或者到“交大門”网站进行[反馈](https://xjtu.men/t/topic/1440)！
+欢迎在Github提Issue或者到“交大門”网站进行[反馈](https://xjtu.men/t/topic/1440)！
 
 更加欢迎有兴趣的同学可以一起开发玩 :hugs:。
 
--- app/src/main/java/men/xjtu/discourse/MainActivity.kt
+++ app/src/main/java/men/xjtu/discourse/MainActivity.kt
@@ -1,29 +1,167 @@
 package men.xjtu.discourse
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import android.view.KeyEvent
+import android.webkit.ValueCallback
+import android.webkit.WebChromeClient
 import android.webkit.WebResourceRequest
 import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.Date
 
 
+private var mCM: String? = null
+private var mUM: ValueCallback<Uri>? = null
+private var mUMA: ValueCallback<Array<Uri>>? = null
+private const val FCR = 1
+private const val FILECHOOSER_RESULTCODE = 1
+
+// upload functionality comes from https://stackoverflow.com/questions/51704560/android-file-chooser-not-calling-from-android-webview/51704638#51704638
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         getSupportActionBar()?.hide()
         setContentView(R.layout.activity_main)
+        val webView: WebView = findViewById(R.id.webview)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.loadWithOverviewMode = true
+        webView.settings.useWideViewPort = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.allowFileAccess = true
+        webView.settings.allowContentAccess = true
+        webView.settings.javaScriptCanOpenWindowsAutomatically = true
+        webView.webViewClient = MyWebViewClient(this)
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onShowFileChooser(
+                webView: WebView,
+                filePathCallback: ValueCallback<Array<Uri>>,
+                fileChooserParams: FileChooserParams
+            ): Boolean {
+                if (mUMA != null) {
+                    mUMA!!.onReceiveValue(null)
+                }
+                mUMA = filePathCallback
+                var takePictureIntent: Intent? = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                if (takePictureIntent!!.resolveActivity(this@MainActivity.getPackageManager()) != null) {
+                    var photoFile: File? = null
+                    try {
+                        photoFile = createImageFile()
+                        takePictureIntent.putExtra("PhotoPath", mCM)
+                    } catch (ex: IOException) {
+                        Log.e("Webview", "Image file creation failed", ex)
+                    }
+                    if (photoFile != null) {
+                        mCM = "file:" + photoFile.getAbsolutePath()
+                        takePictureIntent.putExtra(
+                            MediaStore.EXTRA_OUTPUT,
+                            Uri.fromFile(photoFile)
+                        )
+                    } else {
+                        takePictureIntent = null
+                    }
+                }
+                val contentSelectionIntent = Intent(Intent.ACTION_GET_CONTENT)
+                contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE)
+                contentSelectionIntent.type = "*/*"
+                val intentArray: Array<Intent>
+                intentArray = takePictureIntent?.let { arrayOf(it) } ?: arrayOf<Intent>()
+                val chooserIntent = Intent(Intent.ACTION_CHOOSER)
+                chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent)
+                chooserIntent.putExtra(Intent.EXTRA_TITLE, "Image Chooser")
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray)
+                startActivityForResult(chooserIntent, FCR)
+                return true
+            }
+        }
+        webView.loadUrl("https://xjtu.men")
+    }
 
-        val myWebView: WebView = findViewById(R.id.webview)
-        myWebView.settings.javaScriptEnabled = true
-        myWebView.webViewClient = MyWebViewClient(this)
+    @Throws(IOException::class)
+    private fun createImageFile(): File? {
+        @SuppressLint("SimpleDateFormat") val timeStamp: String =
+            SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageFileName = "img_" + timeStamp + "_"
+        val storageDir: File =
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(imageFileName, ".jpg", storageDir)
+    }
 
-        myWebView.loadUrl("https://xjtu.men")
+    fun openFileChooser(uploadMsg: ValueCallback<Uri?>?) {
+        this.openFileChooser(uploadMsg, "*/*")
+    }
 
+    fun openFileChooser(
+        uploadMsg: ValueCallback<Uri?>?,
+        acceptType: String?
+    ) {
+        this.openFileChooser(uploadMsg, acceptType, null)
+    }
+
+    fun openFileChooser(
+        uploadMsg: ValueCallback<Uri?>?,
+        acceptType: String?,
+        capture: String?
+    ) {
+        val i = Intent(Intent.ACTION_GET_CONTENT)
+        i.addCategory(Intent.CATEGORY_OPENABLE)
+        i.type = "*/*"
+        this@MainActivity.startActivityForResult(
+            Intent.createChooser(i, "File Browser"),
+            FILECHOOSER_RESULTCODE
+        )
+    }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        intent: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, intent)
+        if (Build.VERSION.SDK_INT >= 21) {
+            var results: Array<Uri>? = null
+            //Check if response is positive
+            if (resultCode == Activity.RESULT_OK) {
+                if (requestCode == FCR) {
+                    if (null == mUMA) {
+                        return
+                    }
+                    if (intent == null) { //Capture Photo if no image available
+                        if (mCM != null) {
+                            results = arrayOf(Uri.parse(mCM))
+                        }
+                    } else {
+                        val dataString = intent.dataString
+                        if (dataString != null) {
+                            results = arrayOf(Uri.parse(dataString))
+                        }
+                    }
+                }
+            }
+            mUMA!!.onReceiveValue(results)
+            mUMA = null
+        } else {
+            if (requestCode == FCR) {
+                if (null == mUM) return
+                val result =
+                    if (intent == null || resultCode != Activity.RESULT_OK) null else intent.data
+                mUM!!.onReceiveValue(result)
+                mUM = null
+            }
+        }
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
@@ -41,6 +179,7 @@ class MainActivity : AppCompatActivity() {
         // system behavior (probably exit the activity)
         return super.onKeyDown(keyCode, event)
     }
+
 }
 
 class MyWebViewClient(private val mainActivity: MainActivity) : WebViewClient() {
@@ -48,20 +187,25 @@ class MyWebViewClient(private val mainActivity: MainActivity) : WebViewClient() 
         view: WebView?,
         request: WebResourceRequest?
     ): WebResourceResponse? {
+        return null
         return try {
             when {
                 // intercept XJTU.MEN traffic to use DNS over HTTPS
-                Uri.parse(request?.url.toString()).host == "xjtu.men"  -> WebViewNetworkHandler(request)
+                Uri.parse(request?.url.toString()).host == "xjtu.men" -> WebViewNetworkHandler(
+                    request
+                )
+
                 else -> null
             }
         } catch (e: Exception) {
-            e.printStackTrace();
+            e.printStackTrace()
             null
         }
     }
 
     override fun shouldOverrideUrlLoading(webView: WebView?, url: String): Boolean {
         if (Uri.parse(url).host == "xjtu.men") return false
+        if (Uri.parse(url).host == "2048.xjtu.men") return false
         // open non XJTU.MEN links in system browser
         val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
         mainActivity.startActivity(intent)
